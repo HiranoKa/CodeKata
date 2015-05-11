14544d6098a009d2c2267b11c12547b890ba74f9
HiranoKa <Hirano.katsumi@leadinge.co.jp>
2015-05-11 11:17:49 +0900

    閏年算出を変更（ネスとを浅くした） 入力値が数字以外なら現在の西暦から算出するように変更

--- leap_year.py
+++ leap_year.py
@@ -1,10 +1,21 @@
 __author__ = 'le-user'
 # -*- coding: utf-8 -*-
 
+import datetime
+
 class LeapYear():
-    def __init__(self,  Year = 0):
-        self.Year = Year
+    def __init__(self,  Year = ''):
+        self.Year = self.ChekLInputeapYear(Year)
         self.Rslt = self.GetLeapYear(self.Year)
+
+    def ChekLInputeapYear(self, Year):
+        if Year.isdigit() == False:
+            #数字以外が入っていたら現在の年を再設定する
+            now = datetime.datetime.now()
+            Rslt = int(now.strftime('%Y'))
+        else:
+            Rslt = int(Year)
+        return Rslt
 
     def GetLeapYear(self, Year):
         Rslt = False
@@ -12,12 +23,11 @@ class LeapYear():
             # 0以上が入力されたら判定実行
             if Year % 4 == 0:
                 # 4で割り切れる年を閏年とする
-                if Year % 100 == 0:
-                    # 但し100で割り切れる場合は平年とする
-                    if Year % 400 == 0:
-                        # 但し400で割り切れる場合は閏年になる
-                        Rslt = True
-                else:
+                if Year % 400 == 0:
+                    # 100で割り切れても400で割り切れる年は閏年とする
+                    Rslt = True
+                elif Year % 100 != 0:
+                    # 但し100で割り切れない年のみ閏年とする
                     Rslt = True
         return Rslt
 
@@ -26,8 +36,7 @@ if __name__ == '__main__':
     InputStr = input('西暦で年を入力してください！')
 
     # 閏年の判定
-    InputData = int(InputStr)
-    Func = LeapYear(InputData)
+    Func = LeapYear(InputStr)
     StrWk = str(Func.Year) + '年は、'
     if Func.Rslt == False:
         StrWk += '閏年ではありません！'
