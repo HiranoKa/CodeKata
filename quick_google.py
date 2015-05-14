9b98797eced92dc0e3ba4fd0711cca6dfcb99253
HiranoKa <Hirano.katsumi@leadinge.co.jp>
2015-05-14 12:00:26 +0900

    日付入力方法を修正

--- quick_google.py
+++ quick_google.py
@@ -2,7 +2,7 @@ __author__ = 'le-user'
 # -*- coding: utf-8 -*-
 
 """A quickstart example showing usage of the Google Calendar API."""
-from datetime import datetime
+import datetime
 import os
 
 from apiclient.discovery import build
@@ -10,6 +10,7 @@ from httplib2 import Http
 import oauth2client
 from oauth2client import client
 from oauth2client import tools
+import time
 
 try:
     import argparse
@@ -17,10 +18,10 @@ try:
 except ImportError:
     flags = None
 
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+#SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+SCOPES = 'https://www.googleapis.com/auth/calendar'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Calendar API Quickstart'
-
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -50,17 +51,33 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
+def InputDate(wkDate = ''):
+    if wkDate == '':
+        InpTytle = '開始日を入力してください'
+    else:
+        InpTytle = '終了日を入力してください'
+    InputDate = input(InpTytle + '(yyyy-mm-dd) : ')
+    if InputDate == "":
+        if wkDate == "":
+            tDateTime = datetime.datetime.now()
+            InputDate = tDateTime.strftime('%Y-%m-%d')
+        else:
+            tDateTime = datetime.datetime.strptime(wkDate, '%Y-%m-%d')
+            tDateTime += datetime.timedelta(days=1)
+            InputDate = tDateTime.strftime('%Y-%m-%d')
+    else:
+        tDateTime = datetime.datetime.strptime(InputDate, '%Y-%m-%d')
+        InputDate = tDateTime.strftime('%Y-%m-%d')
+    print(InputDate)
+    return InputDate
 
-def main():
+def EventList(service):
     """Shows basic usage of the Google Calendar API.
 
     Creates a Google Calendar API service object and outputs a list of the next
     10 events on the user's calendar.
     """
-    credentials = get_credentials()
-    service = build('calendar', 'v3', http=credentials.authorize(Http()))
-
-    now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
     print('Getting the upcoming 10 events')
     eventsResult = service.events().list(
         calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
@@ -73,6 +90,83 @@ def main():
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
 
+def EventAdd(service):
+    event = {
+        'start': {'date':'2015-05-14'},
+        'end': {'date':'2015-05-15'},
+        'summary': 'event_from_api'
+    }
+
+    Tytle = input('タイトルを入力 : ')
+    if Tytle != "":
+        event['summary'] = Tytle
+        StrtDate = InputDate('')
+        EndDate = InputDate(StrtDate)
+        if (StrtDate != '') & (EndDate != ''):
+            event['start']['date'] = StrtDate
+            event['end']['date'] = EndDate
+            print('Input event into Google Calendar')
+            calendars = service.calendarList().list().execute()
+            target_calendar_id = calendars['items'][0]['id']
+            created_event = service.events().insert(calendarId=target_calendar_id, body=event).execute()
+        else:
+            print('開始日または終了日を入力してください！')
+    else:
+        print('タイトルを入力してください！！')
+
+def EventOpe(InputDt):
+    credentials = get_credentials()
+    service = build('calendar', 'v3', http=credentials.authorize(Http()))
+
+    if InputDt == '1':
+        #10 events on the user's calendar
+        EventList(service)
+    elif InputDt == '2':
+        # event set
+        EventAdd(service)
+        EventList(service)
+    else:
+        print('unknown input data')
+
+def HoldayList():
+    credentials = get_credentials()
+    service = build('calendar', 'v3', http=credentials.authorize(Http()))
+
+    # 祝祭日一覧取得
+    events = service.events().list(calendarId='ja.japanese#holiday@group.v.calendar.google.com').execute()
+
+    # 祝祭日一覧をソートして表示
+    print('Getting the holiday list')
+    for item in sorted(events['items'], key=lambda item: item['start']['date']):
+        print( u'{0}\t{1}'.format(item['start']['date'], item['summary']))
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+
+    Creates a Google Calendar API service object and outputs a list of the next
+    10 events on the user's calendar.
+    """
+    credentials = get_credentials()
+    service = build('calendar', 'v3', http=credentials.authorize(Http()))
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    eventsResult = service.events().list(
+        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
 
 if __name__ == '__main__':
-    main()
+    InputDt = input('1:イベント一覧/2:イベント追加/3祝祭日一覧：')
+    if (InputDt == '1') | (InputDt == '2'):
+        EventOpe(InputDt)
+    elif InputDt == '3':
+        HoldayList()
+    else:
+        print('unknown input data')
