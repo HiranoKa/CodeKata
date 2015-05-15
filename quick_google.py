dec52acd078211345824517bf062d1262aaa6428
HiranoKa <Hirano.katsumi@leadinge.co.jp>
2015-05-15 18:08:02 +0900

    EventListのHTML出力追加(第二弾)

--- quick_google.py
+++ quick_google.py
@@ -71,6 +71,54 @@ def InputDate(wkDate = ''):
     print(InputDate)
     return InputDate
 
+def EventListGetStrtTm(strDtTm):
+    # 末尾に付加されているTimeZone(+9:00)を削除する
+    strDtDtTmWk = strDtTm.split('+')[0]
+    listDtTmWk = strDtDtTmWk.split('T')
+    strDtDtWk = listDtTmWk[0]
+    tDateTime = datetime.datetime.strptime(strDtDtWk, '%Y-%m-%d')
+    strRet = tDateTime.strftime('%Y/%m/%d')
+    if len(strDtDtTmWk) > 10:
+        # 終日指定されていなければ時間を付与
+        strDtTmWk = listDtTmWk[1]
+        tDateTime = datetime.datetime.strptime(strDtTmWk, '%H:%M:%S')
+        strRet += ' ' + tDateTime.strftime('%H:%S')
+    return strRet
+
+def EventListGetEndTm(strEndTm):
+    # 末尾に付加されているTimeZone(+9:00)を削除する
+    strDtDtTmWk = strEndTm.split('+')[0]
+    listDtTmWk = strDtDtTmWk.split('T')
+    strDtDtWk = listDtTmWk[0]
+    tDateTime = datetime.datetime.strptime(strDtDtWk, '%Y-%m-%d')
+    if len(strDtDtTmWk) <= 10:
+        # 終日指定なら日付を"-1"する
+        tDateTime -= datetime.timedelta(days=1)
+    strRet = tDateTime.strftime('%Y/%m/%d')
+    if len(strDtDtTmWk) > 10:
+        # 終日指定されていなければ時間を付与
+        strDtTmWk = listDtTmWk[1]
+        tDateTime = datetime.datetime.strptime(strDtTmWk, '%H:%M:%S')
+        strRet += ' ' + tDateTime.strftime('%H:%S')
+    return strRet
+
+def EventListGetTm(strDtTm, blEndTm):
+    # 末尾に付加されているTimeZone(+9:00)を削除する
+    strDtDtTmWk = strDtTm.split('+')[0]
+    listDtTmWk = strDtDtTmWk.split('T')
+    strDtDtWk = listDtTmWk[0]
+    tDateTime = datetime.datetime.strptime(strDtDtWk, '%Y-%m-%d')
+    if (blEndTm) & (len(strDtDtTmWk) <= 10):
+        # '終了日時'かつ'終日指定'なら日付を'-1'
+        tDateTime -= datetime.timedelta(days=1)
+    strRet = tDateTime.strftime('%Y/%m/%d')
+    if len(strDtDtTmWk) > 10:
+        # 終日指定されていなければ時間を付与
+        strDtTmWk = listDtTmWk[1]
+        tDateTime = datetime.datetime.strptime(strDtTmWk, '%H:%M:%S')
+        strRet += ' ' + tDateTime.strftime('%H:%S')
+    return strRet
+
 def EventListHtml():
     Fname = 'EventList.html'
     credentials = get_credentials()
@@ -83,11 +131,11 @@ def EventListHtml():
     f = open(Fname, 'w')
 
     # ヘッダー
-    f.write('''
-   <html>
-   <head></head>
-   <body>
-      <ul>\n''')
+    f.write('<html>\n')
+    f.write('  <head>\n')
+    f.write('    <tytle><B>Googleカレンダー('+target_calendar_id+')</B></tytle>\n')
+    f.write('  </head>\n')
+    f.write('  <body>\n')
 
     #　イベント一覧取得
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
@@ -95,25 +143,21 @@ def EventListHtml():
         calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
         orderBy='startTime').execute()
     events = eventsResult.get('items', [])
-
-    f.write('          <table border="1" cellpadding="1" cellspacing="1" class="event_list">\n')
-    f.write('             <tr><th colspan="3" class="event_list">EventList(' + target_calendar_id + ')</th></tr>\n')
-    f.write('             <tr><th class="tytle">TYTLE</th><th class="StrtTm">START</th><th class="EndTm">END</th></tr>\n')
+    f.write('    <hr>')
     if not events:
-        f.write('             <tr><td class="noday">&nbsp;</td><td class="noday">&nbsp;</td></td><td class="noday">&nbsp;</td></tr>\n')
-    for event in events:
-        strtTm = event['start'].get('dateTime', event['start'].get('date'))
-        endTm = event['end'].get('dateTime', event['end'].get('date'))
-        tytle = event['summary']
-        f.write('             <tr><td class="tytle">' + tytle + '</td><td class="StrtTm">' + strtTm + '</td><td class="EndTm">' + endTm +'</td></tr>\n')
+        f.write('    <p>No upcoming events found.</p>\n')
 
-    f.write('''          </table>''')
+    f.write('    <table border="0" cellpadding="5" cellspacing="0" aligin = "left" class="event_list">\n')
+    for event in events:
+        strtTm = EventListGetTm(event['start'].get('dateTime', event['start'].get('date')), False)
+        endTm = EventListGetTm(event['end'].get('dateTime', event['end'].get('date')), True)
+        tytle = event['summary']
+        f.write('      <tr><td class="tytle">' + tytle + '</td><td class="StrtTm">' + strtTm + '</td><td class="EndTm">' + endTm +'</td></tr>\n')
+    f.write('    </table>\n')
 
     # フッター
-    f.write('''
-       </ul>
-   </body>
-   </html>''')
+    f.write('  </body>\n')
+    f.write('</html>\n')
 
     # 終了
     f.close()
@@ -135,7 +179,20 @@ def EventList(service):
         print('No upcoming events found.')
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+        endTm = event['end'].get('dateTime', event['end'].get('date'))
+        strtTm = EventListGetTm(start, False) + ' - ' + EventListGetTm(endTm, True)
+        print(strtTm, event['summary'])
+
+def EventList2(service):
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    eventsResult = service.events().list(
+        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+
+
 
 def EventAdd(service):
     event = {
@@ -168,7 +225,7 @@ def EventOpe(InputDt):
     if InputDt == '1':
         #10 events on the user's calendar
         EventList(service)
-    elif InputDt == '2':
+    elif InputDt == '3':
         # event set
         EventAdd(service)
         EventList(service)
@@ -235,12 +292,12 @@ def main():
         print(start, event['summary'])
 
 if __name__ == '__main__':
-    InputDt = input('1:イベント一覧/2:イベント追加/3:イベント一覧(HTML)/4:祝祭日一覧/5:祝祭日一覧(HTML)：')
-    if (InputDt == '1') | (InputDt == '2'):
+    InputDt = input('1:イベント一覧/2:イベント一覧(HTML)/3:イベント追加/4:祝祭日一覧/5:祝祭日一覧(HTML)：')
+    if (InputDt == '1') | (InputDt == '3'):
         EventOpe(InputDt)
-    elif InputDt == '3':
+    elif InputDt == '2':
         EventListHtml()
-    elif (InputDt == '4') | (InputDt == '5') :
+    elif (InputDt == '4') | (InputDt == '5'):
         HoldayList(InputDt)
     else:
         print('unknown input data')
